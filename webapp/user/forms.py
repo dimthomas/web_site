a5f50e4da7209516d4905b6fcd0a4d99f7fcf313
Dmitry Averyanov <dimthomas1@mail.ru>
2020-04-05 15:54:55 +0300

    News from Habrahabr and Celery support

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -4,17 +4,19 @@ from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
 from webapp.user.models import User
 
+
 class LoginForm(FlaskForm):
     username = StringField('Имя пользователя', validators=[DataRequired()], render_kw={"class": "form-control"})
     password = PasswordField('Пароль', validators=[DataRequired()], render_kw={"class": "form-control"})
     remember_me = BooleanField('Запомнить меня', default=True, render_kw={"class": "form-check-input"})
     submit = SubmitField('Отправить!', render_kw={"class": "btn btn-primary"})
 
+
 class RegistrationForm(FlaskForm):
     username = StringField('Имя пользователя', validators=[DataRequired()], render_kw={"class": "form-control"})
     email = StringField('Электронная почта', validators=[DataRequired(), Email()], render_kw={"class": "form-control"})
     password = PasswordField('Пароль', validators=[DataRequired()], render_kw={"class": "form-control"})
-    password2 = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')], render_kw={"class": "form-control"})
+    password2 = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password', message='Пароли не равны')], render_kw={"class": "form-control"})
     submit = SubmitField('Отправить!', render_kw={"class": "btn btn-primary"})
 
     def validate_username(self, username):

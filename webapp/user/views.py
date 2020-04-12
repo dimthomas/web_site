e94cd4c0597011cfc3ef5bb68afb49ba41fce8ae
Dmitry Averyanov <dimthomas1@mail.ru>
2020-04-12 16:21:22 +0300

    Добавлена возможность оставлять комментарии на сайте для авторизованных пользователей

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -4,16 +4,19 @@ from flask_login import current_user, login_user, logout_user
 from webapp.db import db
 from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.models import User
+from webapp.utils import get_redirect_target
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
+
 
 @blueprint.route('/login')
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('news.index'))
+        return redirect(get_redirect_target())
     title = 'Авторизация'
     login_form = LoginForm()
     return render_template('user/login.html', page_title=title, form=login_form)
+
 
 @blueprint.route('/process-login', methods=['POST'])
 def process_login():
@@ -24,15 +27,18 @@ def process_login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             flash('Вы успешно вошли на сайт')
-            return redirect(url_for('news.index'))
+            return redirect(get_redirect_target())
+
     flash('Неправильные имя пользователя или пароль')
     return redirect(url_for('user.login'))
+
 
 @blueprint.route('/logout')
 def logout():
     logout_user()
     flash('Вы успешно разлогинились')
     return redirect(url_for('news.index'))
+
 
 @blueprint.route('/register')
 def register():
@@ -41,6 +47,7 @@ def register():
     title = 'Регистрация'
     form = RegistrationForm()
     return render_template('user/registration.html', page_title=title, form=form)
+
 
 @blueprint.route('/process-reg', methods=['POST'])
 def process_reg():
